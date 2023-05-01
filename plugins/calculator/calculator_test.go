254644b5cfa897032bdcc710bc69c83c7c352d0e
river <laolei@forkway.cn>
2023-05-01 11:47:29 +0800

    refactor: add test file

---
+++ plugins/calculator/calculator_test.go
@@ -0,0 +1,37 @@
+package calculator
+
+import (
+	"context"
+	"testing"
+)
+
+func TestCalculator_Do(t *testing.T) {
+	ts := []struct {
+		testname string
+		query    string
+		expect   string
+	}{
+		{
+			"Add",
+			"1+1",
+			"2",
+		},
+		{
+			"Sub",
+			"1-1",
+			"0",
+		},
+	}
+	for _, tt := range ts {
+		t.Run(tt.testname, func(t *testing.T) {
+			c := NewCalculator()
+			got, err := c.Do(context.Background(), tt.query)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if got != tt.expect {
+				t.Errorf("Calculator.Do() = %v, want %v", got, tt.expect)
+			}
+		})
+	}
+}
